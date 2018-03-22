3d697d62dbc1b414e159b1cd9a903e44820c8752
Venesmaa <klaus.venesmaa@accenture.com>
2018-03-22 23:36:50 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -28,6 +29,30 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+// GET /todos/12345
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  // If invalid id then 400 and return empty body
+  if (!ObjectID.isValid(id)) {
+    return res.status(400).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    // If no todo then 404 and return empty body
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    // If error then 400 and return empty body
+    res.status(400).send();
+  });
+
+  // findbyid -> success case send back if no todo 404 or error case 400 empty body
+})
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
