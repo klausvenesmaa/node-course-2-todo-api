6cd71d35ba4a5a60c63e9a450589db5016744a8f
Venesmaa <klaus.venesmaa@accenture.com>
2018-03-25 20:05:54 +0300

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,17 +1,32 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc';
 
-var token = jwt.sign(data, '123abc');
+// bcrypt.genSalt(10, (err,salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-console.log(token);
+var hashedPassword = '$2a$10$Q4iKpFME6q5coSjqOGC1aeZLAlxROaHI/hAEMOdUvF131QzUvtAH.';
 
-var decoded = jwt.verify(token, '123abc');
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
 
-console.log(decoded);
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+//
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+//
+// console.log(decoded);
 
 
 // var message = 'I am user number 3';
