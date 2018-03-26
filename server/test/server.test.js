91aca04d3be99307a5ed4fad28535ac07327d601
Venesmaa <klaus.venesmaa@accenture.com>
2018-03-26 21:13:18 +0300

    Add tests for POST /users/login

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -247,7 +247,7 @@ describe('POST /users', () => {
           expect(user).toBeTruthy();
           expect(user.password === password).toBeFalsy();
           done();
-        });
+        }).catch((e) => done(e));
       });
   });
 
@@ -294,6 +294,57 @@ describe('POST /users', () => {
           expect(users.length).toBe(2);
           done();
         });
+      });
+  });
+});
+
+describe('POST /users/login', () => {
+  it('should login user and return token', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toEqual(expect.any(String));
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens[0].toObject()).toMatchObject({
+            access: 'auth',
+            token: res.headers['x-auth']
+          });
+          done();
+        }).catch((e) => done(e));
+      })
+  });
+
+  it('should reject invalid login', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password + 'NotValid'
+      })
+      .expect(400)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toBeUndefined();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
       });
   });
 });
